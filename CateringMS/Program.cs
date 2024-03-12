a1af2c63fe35cb07c454e09dd2c8725241d906f2
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-03-12 14:18:01 +0800

    Button

--- CateringMS/Program.cs
+++ CateringMS/Program.cs
@@ -32,6 +32,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Login}/{action=Index}/{id?}");
+    pattern: "{controller=CateringOrder}/{action=Create}/{id?}");
 
 app.Run();
