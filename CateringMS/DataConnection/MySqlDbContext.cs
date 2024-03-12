35a428c19632cf6ef59deddf814b397639a1636e
Zarathustra09 <81860905+Zarathustra09@users.noreply.github.com>
2024-03-12 09:35:26 +0800

    Add project files.

---
+++ CateringMS/DataConnection/MySqlDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySqlConnector;
+using CateringMS.Models;
+
+namespace CateringMS.DataConnection
+{
+    public class MySqlDbContext:DbContext
+    {
+ 
+      
+
+        public MySqlDbContext(DbContextOptions<MySqlDbContext> options) : base(options) { }
+    }
+}
